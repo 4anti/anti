15387e063f5404ba7831d7e8b83a0d3a7566a3e1
4nti <burakkural07@gmail.com>
2025-10-16 00:54:59 +0300

    Update index.html and style.css

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 console.log("Antifalls Landing Page");
 
-// Loading Screen
+// Loading Screen Blur & Fade
 const element = document.getElementById("site-loading-header");
 
+let blurPower = 15;
+let delay = 1;
+let opacity = 0;
+element.style.opacity = opacity;
+let translateY = 50;
+element.style.filter = "blur(" + blurPower + "px)";
 
-
+function removeBlurFade() {
+    if (blurPower > 0) {
+        element.style.filter = "blur(" + blurPower + "px)";
+        element.style.opacity = opacity;
+        blurPower = blurPower - 0.12;
+        opacity = opacity + 0.01;
+        setTimeout(removeBlurFade, delay);
+    }
+}
+removeBlurFade();
 
 
 
@@ -14,4 +29,10 @@ const element = document.getElementById("site-loading-header");
 //const scalingFactors = {"BBH Sans Hegarty": 1.0, "Gravitas One": 0.95, "Poller One": 1, "Michroma": 0.85, "Bodoni Moda": 1};
 //let delay = 50;
 //let currentindex = 0;
-//function cycleFonts() {const fontName = fonts[currentindex];element.style.fontFamily = fontName;element.style.transform = `translate(-50%, -50%) scale(${scalingFactors[fontName]})`;currentindex = (currentindex + 1) % fonts.length;delay = delay * 1.3;if (delay <= 500) {setTimeout(cycleFonts, delay);}}
+//function cycleFonts() {
+// const fontName = fonts[currentindex];
+// element.style.fontFamily = fontName;
+// currentindex = (currentindex + 1) % fonts.length;
+// delay = delay * 1.3;
+// if (delay <= 500) {setTimeout(cycleFonts, delay)
+// ;}}
